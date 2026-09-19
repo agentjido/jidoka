9191d590cfe60c711dd66061a3776e0e59d80077
Mike Hostetler <84222+mikehostetler@users.noreply.github.com>
2026-09-19 11:12:01 -0500

    deps: update ReqLLM, LLMDB, and Runic

--- test/jidoka/runtime/req_llm_test.exs
+++ test/jidoka/runtime/req_llm_test.exs
@@ -58,7 +58,7 @@ end
 defmodule Jidoka.Adapter.ReqLLMTest do
   use ExUnit.Case, async: true
 
-  @supported_req_llm "~> 1.21.0"
+  @supported_req_llm "~> 1.24"
 
   alias Jidoka.Adapter.ReqLLM
   alias Jidoka.Adapter.ReqLLM.ResponseAdapter
